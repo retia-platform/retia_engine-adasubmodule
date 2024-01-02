58e9aa6a1133a6307efdb85c1382931587a52346
salahudin-24 <salahudinalayubi24@gmail.com>
2024-01-02 17:06:58 +0700

    Fix: IP address replacement

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from operations import interface
 
 
@@ -8,4 +9,5 @@ global_header={"Content-Type": "application/yang-data+json", "Accept": "applicat
 
 # print(getInterface(global_ipaddr, global_port, global_auth, global_header))
 # print(getInterfaceList(global_ipaddr, global_port, global_auth, global_header))
-print(interface.getList(global_ipaddr, global_port, global_auth, global_header))
+print(interface.set(global_ipaddr,global_port, global_auth, global_header,"GigabitEthernet2",{"description":"ini desc","ip":"10.10.10.1", "netmask":"255.255.255.0", "enabled":"false"}))
+# print(interface.getList(global_ipaddr, global_port, global_auth, global_header))
--- operations/interface.py
+++ operations/interface.py
@@ -33,31 +33,53 @@ def get(ipaddr: str, port: str,  credential: tuple, header: dict, interface_name
 
     return {"code" : response_code, "body" : response_body}
     
-def set(ipaddr: str, port: str,  credential: tuple, header: dict, interface_name: str, req_to_change: dict)->dict:
-    update_body={"ietf-ip:ipv4":{"address":[{}]}}
-    if "description" in req_to_change:
-        update_body.update({"description":req_to_change["description"]})
-    if "enabled" in req_to_change:
-        update_body.update({"enabled":req_to_change["enabled"]})
-    if "ip" in req_to_change:
-        # update_body.update({"ietf-ip:ipv4":{"address":[{"ip":req_to_change["ip"]}]}})
-        update_body["ietf-ip:ipv4"]["address"][0]["ip"]=req_to_change["ip"]
-    if "netmask" in req_to_change:
-        update_body["ietf-ip:ipv4"]["address"][0]["netmask"]=req_to_change["netmask"]
-    
+def set(ipaddr: str, port: str,  credential: tuple, header: dict, interface_name: str, req_to_change: dict):
     target_url=urlGenerator(ipaddr, port, "ietf-interfaces", "interfaces", "interface", interface_name)
-    update_body={"ietf-interfaces:interface":update_body}
-    update_body=json.dumps(update_body, indent=2)
-    try:
-        if len(interface_name) > 1:
-            response_body=json.loads(response.text)
-            response_body=response_body["ietf-interfaces:interface"]
-        else:
-            response_body=json.loads(response.text)
-            response_body=response_body["ietf-interfaces:interfaces"]["interface"]
-    except:
-        response_body = {}
+    update_body=get(ipaddr, port, credential, header, interface_name)["body"]
+    # Interface Description
+    if len(req_to_change["description"])>0:
+        update_body["description"]=req_to_change["description"]
+    else:
+        if "description" in update_body:
+            del update_body["description"]
 
-    response = requests.patch(url=target_url, auth=credential, headers=header, data=update_body, verify=False)
-    return {"code" : response.status_code, "body" : response_body}
+    # Interface Addr
+    if len(req_to_change["ip"])>0 and len(req_to_change["netmask"])>0:
+        if "address" in update_body:
+            del update_body["ietf-ip:ipv4"]["address"]    
+        update_body["ietf-ip:ipv4"]={"address":[{}]}
+        update_body["ietf-ip:ipv4"]["address"][0]["ip"]=req_to_change["ip"]
+        update_body["ietf-ip:ipv4"]["address"][0]["netmask"]=req_to_change["netmask"]
+    else:
+        del update_body["ietf-ip:ipv4"]["address"]
+
+    # Interface enable
+    update_body["enabled"]=req_to_change["enabled"]
+
+    update_body=json.dumps({"ietf-interfaces:interface":update_body})
+    response=requests.put(url=target_url, auth=credential, headers=header, data=update_body, verify=False)
+    return {"code": response.status_code, "body":response.text}
+
+    
+
+
+
+
+
+    # target_url=urlGenerator(ipaddr, port, "ietf-interfaces", "interfaces", "interface", interface_name)
+    # update_body=json.dumps({"ietf-interfaces:interface":update_body})
+    # print(update_body)
+    # response = requests.put(url=target_url, auth=credential, headers=header, data=update_body, verify=False)
+
+    # try:
+    #     if len(interface_name) > 1:
+    #         response_body=json.loads(response.text)
+    #         response_body=response_body["ietf-interfaces:interface"]
+    #     else:
+    #         response_body=json.loads(response.text)
+    #         response_body=response_body["ietf-interfaces:interfaces"]["interface"]
+    # except:
+    #     response_body = {}
+
+    # return {"code" : response.status_code, "body" : response_body}
 
