63479d61d56b2e0745ff235ab367eb7389f6630f
salahudin-24 <salahudinalayubi24@gmail.com>
2024-01-02 23:22:55 +0700

    feat: get static routes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from operations import interface
+from operations import interface, static_routing
 
 
 global_ipaddr="192.168.137.1"
@@ -9,4 +9,5 @@ global_header={"Content-Type": "application/yang-data+json", "Accept": "applicat
 # print(getInterface(global_ipaddr, global_port, global_auth, global_header))
 # print(getInterfaceList(global_ipaddr, global_port, global_auth, global_header))
 # print(interface.set(global_ipaddr,global_port, global_auth, global_header,"GigabitEthernet2",{"description":"ini desc","ip":"10.10.10.1", "netmask":"255.255.255.0", "enabled":"false"}))
-print(interface.get(global_ipaddr, global_port, global_auth, global_header))
+# print(interface.get(global_ipaddr, global_port, global_auth, global_header))
+print(static_routing.get(global_ipaddr, global_port, global_auth, global_header))
--- operations/static_routing.py
+++ operations/static_routing.py
@@ -1,3 +1,10 @@
-from url_generator import urlGenerator
+from operations.url_generator import urlGenerator
+import requests, json
 
-def 
+def get(ipaddr: str, port: str,  credential: tuple, header: dict):
+    target_url = "https://%s:%s/restconf/data/ietf-routing:routing/routing-instance=default/routing-protocols/routing-protocol"%(ipaddr,port)
+    response = requests.get(url=target_url, auth=credential, headers=header, verify=False)
+    response_code=response.status_code
+    response_body=json.loads(response.text)
+    response_body=response_body["ietf-routing:routing-protocol"][0]["static-routes"]
+    print(response_body)
