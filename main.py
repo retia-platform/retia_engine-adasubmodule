5f5c2328634896e47474acdcea706f3d024ed0c3
salahudin-24 <salahudinalayubi24@gmail.com>
2024-01-06 01:46:50 +0700

    feat: ospf

--- main.py
+++ main.py
@@ -12,5 +12,7 @@ global_header={"Content-Type": "application/yang-data+json", "Accept": "applicat
 # print(interface.get(global_ipaddr, global_port, global_auth, global_header))
 # print(static_routing.set(global_ipaddr, global_port, global_auth, global_header, [{"destination-prefix":"0.0.0.0/0", "next-hop-address":"192.168.137.2"}]))
 
-print(setPassEncryption({"ipaddr": global_ipaddr, "port": global_port, "credential": global_auth}, {"password-encryption":"true"}))
-# print(getPassEncryption({"ipaddr": global_ipaddr, "port": global_port, "credential": global_auth}))
+# print(createOspfProcess({"ipaddr": global_ipaddr, "port": global_port, "credential": global_auth}, {"id":10}))
+# print(setOspfProcessDetail({"ipaddr": global_ipaddr, "port": global_port, "credential": global_auth}, {"id":10, "default-information-originate":"true", "network":[{"ip":"10.0.0.4","wildcard":"0.0.0.3","area":0},{"ip":"10.0.0.8","wildcard":"0.0.0.3","area":0}],"passive-interface":["GigabitEthernet1","GigabitEthernet4","GigabitEthernet5"],"redistribute":["connected"]}))
+# print(delOspfProcess({"ipaddr": global_ipaddr, "port": global_port, "credential": global_auth}, {"id":10}))
+print(getOspfProcessDetail({"ipaddr": global_ipaddr, "port": global_port, "credential": global_auth},{"id":10}))
--- operation.py
+++ operation.py
@@ -9,9 +9,19 @@ def patchSomething(conn_strings: dict, path: str, body: str):
     target_url="https://%s:%s/restconf/data/Cisco-IOS-XE-native:native%s"%(conn_strings["ipaddr"], conn_strings["port"], path)
     return requests.patch(url=target_url, auth=conn_strings["credential"], headers={"Content-Type": "application/yang-data+json", "Accept": "application/yang-data+json"}, data=body, verify=False)
 
+def putSomething(conn_strings: dict, path: str, body: str):
+    target_url="https://%s:%s/restconf/data/Cisco-IOS-XE-native:native%s"%(conn_strings["ipaddr"], conn_strings["port"], path)
+    return requests.put(url=target_url, auth=conn_strings["credential"], headers={"Content-Type": "application/yang-data+json", "Accept": "application/yang-data+json"}, data=body, verify=False)
+
+def postSomething(conn_strings: dict, path: str, body: str):
+    target_url="https://%s:%s/restconf/data/Cisco-IOS-XE-native:native%s"%(conn_strings["ipaddr"], conn_strings["port"], path)
+    return requests.post(url=target_url, auth=conn_strings["credential"], headers={"Content-Type": "application/yang-data+json", "Accept": "application/yang-data+json"}, data=body, verify=False)
+
 def delSomething(conn_strings: dict, path: str):
     target_url="https://%s:%s/restconf/data/Cisco-IOS-XE-native:native%s"%(conn_strings["ipaddr"], conn_strings["port"], path)
     return requests.delete(url=target_url, auth=conn_strings["credential"], headers={"Content-Type": "application/yang-data+json", "Accept": "application/yang-data+json"}, verify=False)
+
+
 
 
 def getVersion(conn_strings: dict)->dict:
@@ -27,15 +37,19 @@ def setHostname(conn_strings: dict, req_to_change: dict)->dict:
     response=patchSomething(conn_strings, "/hostname", body)
     return {"code": response.status_code, "body": response.text}
 
-def getLoginBanner(conn_strings)->dict:
+def getLoginBanner(conn_strings: dict)->dict:
     response=getSomething(conn_strings, "/banner/login")
-    try:
-        response_body=json.loads(response.text)["Cisco-IOS-XE-native:login"]["banner"]
-    except:
+
+    if len(response.text)>0:
+        try:
+            response_body=json.loads(response.text)["Cisco-IOS-XE-native:login"]["banner"]
+        except:
+            response_body=json.loads(response.text)
+    else:
         response_body={}
     return {"code": response.status_code, "body": response_body}
 
-def setLoginBanner(conn_strings, req_to_change: dict):
+def setLoginBanner(conn_strings: dict, req_to_change: dict)->dict:
     if len(req_to_change["banner_login"])>0:
         body=json.dumps({"login": {"banner": req_to_change["banner_login"]}},indent=2)
         response=patchSomething(conn_strings, "/banner/login", body)
@@ -49,28 +63,31 @@ def setLoginBanner(conn_strings, req_to_change: dict):
 
     return {"code": response.status_code, "body": response_body}
 
-def getMotdBanner(conn_strings)->dict:
+def getMotdBanner(conn_strings:dict)->dict:
     response=getSomething(conn_strings, "/banner/motd")
-    try:
-        response_body=json.loads(response.text)["Cisco-IOS-XE-native:motd"]["banner"]
-    except:
+
+    if len(response.text)>0:
+        try:
+            response_body=json.loads(response.text)["Cisco-IOS-XE-native:motd"]["banner"]
+        except:
+            response_body=json.loads(response.text)
+    else:
         response_body={}
     return {"code": response.status_code, "body": response_body}
 
-def setMotdBanner(conn_strings, req_to_change: dict):
+def setMotdBanner(conn_strings:dict, req_to_change: dict)->dict:
     if len(req_to_change["banner_motd"])>0:
         body=json.dumps({"motd": {"banner": req_to_change["banner_motd"]}},indent=2)
         response=patchSomething(conn_strings, "/banner/motd", body)
     else:
         response=delSomething(conn_strings, "/banner/motd")
-        
     try:
         response_body=json.loads(response.text)
     except:
         response_body={}
     return {"code": response.status_code, "body": response_body}
 
-def getPassEncryption(conn_strings):
+def getPassEncryption(conn_strings:dict)->dict:
     response=getSomething(conn_strings, "/service/")
     if "password-encryption" in json.loads(response.text)["Cisco-IOS-XE-native:service"]:
         response_body="true"
@@ -91,3 +108,87 @@ def setPassEncryption(conn_strings, req_to_change: dict):
         response_body={}
 
     return {"code": response.status_code, "body": response_body}
+
+def getStaticRoute(conn_strings: dict)->dict:
+    response=getSomething(conn_strings, "/ip/route/ip-route-interface-forwarding-list")
+    if len(response.text)>0:
+        try:
+            response_body=json.loads(response.text)["Cisco-IOS-XE-native:ip-route-interface-forwarding-list"]
+        except:
+            response_body=json.loads(response.text)
+    else:
+        response_body={}
+    return {"code": response.status_code, "body": response_body}
+
+def setStaticRoute(conn_strings: dict, req_to_change: list)->dict:
+    body=json.dumps({"Cisco-IOS-XE-native:route":{"ip-route-interface-forwarding-list": req_to_change}}, indent=2)
+    response=putSomething(conn_strings, "/ip/route", body )
+    try:
+        response_body=json.loads(response.text)
+    except:
+        response_body={}
+    return {"code": response.status_code, "body": response_body}
+
+
+def getOspfProcesses(conn_strings: dict)->list:
+    response=getSomething(conn_strings, "/router/Cisco-IOS-XE-ospf:router-ospf/ospf/process-id?fields=id")
+    if len(response.text)>0:
+        try:
+            router_id=json.loads(response.text)["Cisco-IOS-XE-ospf:process-id"]
+            response_body=[]
+            for id in router_id:
+                response_body.append(id["id"])
+        except:
+            response_body=json.loads(response.text)
+    else:
+        response_body=[]
+    return {"code": response.status_code, "body": response_body}
+
+def createOspfProcess(conn_strings: dict, req_to_create: dict):
+    body=json.dumps({"Cisco-IOS-XE-ospf:process-id":{"id": req_to_create["id"]}}, indent=2)
+    response=postSomething(conn_strings, "/router/Cisco-IOS-XE-ospf:router-ospf/ospf/", body)
+    try:
+        response_body=json.loads(response.text)
+    except:
+        response_body={}
+    return {"code": response.status_code, "body": response_body}
+
+def delOspfProcess(conn_strings: dict, req_to_del:dict)->list:
+    response=delSomething(conn_strings, "/router/Cisco-IOS-XE-ospf:router-ospf/ospf/process-id=%s"%(req_to_del["id"]))
+    try:
+        response_body=json.loads(response.text)
+    except:
+        response_body={}
+    return {"code": response.status_code, "body": response_body}
+
+def getOspfProcessDetail(conn_strings: dict, req_to_show: dict)->dict:
+    response=getSomething(conn_strings, "/router/Cisco-IOS-XE-ospf:router-ospf/ospf/process-id=%s"%(req_to_show["id"]))
+    if len(response.text)>0:
+        try:
+            response_body=json.loads(response.text)["Cisco-IOS-XE-ospf:process-id"]
+        except:
+            response_body=json.loads(response.text)
+    else:
+        response_body={}
+    return {"code": response.status_code, "body": response_body}
+
+def setOspfProcessDetail(conn_strings: dict, req_to_change: dict)->dict:
+    body={"Cisco-IOS-XE-ospf:process-id":{"id":req_to_change["id"], "network":req_to_change["network"], "passive-interface": {"interface": req_to_change["passive-interface"]}}}
+
+    if req_to_change["default-information-originate"]=="true":
+        body["Cisco-IOS-XE-ospf:process-id"]["default-information"]={"originate":{}}
+
+    if len(req_to_change["redistribute"])>0:
+        body["Cisco-IOS-XE-ospf:process-id"]["redistribute"]={}
+        for r in req_to_change["redistribute"]:
+            body["Cisco-IOS-XE-ospf:process-id"]["redistribute"][r]={}
+
+
+    body=json.dumps(body, indent=2)
+    print(body)
+
+    response=putSomething(conn_strings, "/router/Cisco-IOS-XE-ospf:router-ospf/ospf/process-id=%s"%(req_to_change["id"]), body)
+
+    return response
+    
+
